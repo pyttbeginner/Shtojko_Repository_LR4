125371eb1b4d9e70d5cbff6cbc7d9293dd2735da
pyttbeginner <fomenckodimon783@yandex.ru>
2025-04-08 16:16:58 +0300

    switch-case added

--- Shtojko_project_LR4.cpp
+++ Shtojko_project_LR4.cpp
@@ -15,19 +15,39 @@ double convertToGigabytes(double bytes) {
 
 
 int main() {
+    
+    
    
+        static double bytes = 0; 
+        
         switch (choice) {
             case 1:
-             
+                bytes = getInputInBytes();
+                cout << "Введено: " << bytes << " байт\n";
+                break;
+                
             case 2:
+                if (bytes == 0) {
+                    cout << "Сначала введите объем данных!\n";
+                    break;
+                }
+                cout << "Результат: " << convertToMegabytes(bytes) << " MB\n";
+                break;
                 
             case 3:
-               
+                if (bytes == 0) {
+                    cout << "Сначала введите объем данных!\n";
+                    break;
+                }
+                cout << "Результат: " << convertToGigabytes(bytes) << " GB\n";
+                break;
+                
             case 0:
+                run = false;
+                break;
                 
             default:
-              
-    }
+                cout << "Неверный выбор!\n";
     
     cout << "Программа завершена.\n";
     
