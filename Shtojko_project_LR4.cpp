62ad51c5913a3ef6a430e91686109d59c6fadedb
pyttbeginner <110183548+pyttbeginner@users.noreply.github.com>
2025-04-08 15:25:58 +0300

    Create Shtojko_project_LR4.cpp

---
+++ Shtojko_project_LR4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+// Функция для получения объема данных в байтах
+double getInputInBytes() {
+}
+
+double convertToMegabytes(double bytes) {
+
+}
+
+double convertToGigabytes(double bytes) {
+   
+}
+
+
+int main() {
+   
+        switch (choice) {
+            case 1:
+             
+            case 2:
+                
+            case 3:
+               
+            case 0:
+                
+            default:
+              
+    }
+    
+    cout << "Программа завершена.\n";
+    
+    return 0;
+}
